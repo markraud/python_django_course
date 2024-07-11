abb59ade07c0e65423909b673fa72f2437a7d0ff
Mark Raudenbush <markraud@hotmail.com>
2024-07-10 20:21:06 -0600

    added path and view for /challenges

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 # Create your views here.
 
 monthly_challenges ={
-    'january': 'challenge for January!',
+    'january': 'Challenge for January!',
     'february': 'Challenge for February!',
     'march': 'Challenge for March!',
     'april': 'Challenge for April!',
@@ -18,10 +18,20 @@ monthly_challenges ={
     'december': 'Challenge for December!'
 }
 
+def index(request):
+    list_items = '' 
+    months = list(monthly_challenges.keys())
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse('month-challenge', args=[month])
+        list_items += f'<li><a href=\'{month_path}\'>{capitalized_month}</a></li>'
+        response_data = f'<ul>{list_items}</ul'
+    return HttpResponse(response_data)
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
-        return HttpResponseNotFound('Invalid Month: This month is not supported!')
+        return HttpResponseNotFound('<h1>Invalid Month: This month is not supported!</h1>')
     redirect_month = months[month - 1]
     redirect_path = reverse('month-challenge', args=[redirect_month])  # build /challenge/january
     return HttpResponseRedirect(redirect_path)
@@ -29,6 +39,8 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
+        response_data = f'<h1>{challenge_text}</h1>'
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound('This month is not supported!')
+        return HttpResponseNotFound('<h1>This month is not supported!</h1>')
     return HttpResponse(challenge_text)
