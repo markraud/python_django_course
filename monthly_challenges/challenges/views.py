80c55066dd4af92c9b1516632489bd3940363534
Mark Raudenbush <markraud@hotmail.com>
2024-07-17 10:42:24 -0600

    template inheritance and partial template snippets

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.http import Http404, HttpResponseRedirect, HttpResponseNotFound
 from django.urls import reverse
 
 # Create your views here.
@@ -16,27 +16,25 @@ monthly_challenges = {
     'september': 'Practice Django for 20 minutes a day - September!',
     'october': 'Practice Django for 20 minutes a day - October!',
     'november': 'Practice Django for 20 minutes a day - November!',
-    'december': 'Practice Django for 20 minutes a day - December!'
+    'december': None
+    # 'december': 'Practice Django for 20 minutes a day - December!'
 }
 
 
 def index(request):
-    list_items = ''
+    # list_items = ''
     months = list(monthly_challenges.keys())
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse('month-challenge', args=[month])
-        list_items += f'<li><a href=\'{month_path}\'>{capitalized_month}</a></li>'
-        response_data = f'<ul>{list_items}</ul'
-    return HttpResponse(response_data)
+    return render(request, 'challenges/index.html', {
+        'months': months
+    })
 
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
         return HttpResponseNotFound('<h1>Invalid Month: This month is not supported!</h1>')
-    redirect_month = months[month - 1]
     # build /challenge/[month] dynamically
+    redirect_month = months[month - 1]    
     redirect_path = reverse('month-challenge', args=[redirect_month])
     return HttpResponseRedirect(redirect_path)
 
@@ -44,10 +42,9 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request,'challenges/challenge.html',{
+        return render(request, 'challenges/challenge.html', {
             'text': challenge_text,
-            'month_name':month.capitalize()
+            'month_name': month
         })
     except:
-        return HttpResponseNotFound('<h1>This month is not supported!</h1>')
-    return HttpResponse(challenge_text)
+        raise Http404()
